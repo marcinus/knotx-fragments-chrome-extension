e682c3745a3c96264a0a70db6a2fd30c6401c8be
Szymon Szajek <szajekszymon.personal@gmail.com>
2020-04-01 12:17:00 +0200

    add eslint rule and add missing header (#60)
    #42 add eslint rule and add missing header

--- header.config.js
+++ header.config.js
@@ -13,15 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import { Network } from 'vis-network';
-import { drawGraph } from './drawingHelper';
-import * as mock from './declarationHelper.mock';
-import { constructGraph } from './declarationHelper';
-
-test('drawing a graph returns a vis.Network object', () => {
-  const mockElement = document.createElement('div');
-  const graph = constructGraph(mock.singleNode);
-
-  expect(drawGraph(graph, mockElement)).toBeInstanceOf(Network);
-});
